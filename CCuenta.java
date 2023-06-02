98229996957b9e2a7dace923e39d8a97c9876d9f
JonathanBetPer <jonathanbetancorperdomo@gmail.com>
2023-06-02 17:19:52 +0100

    Código corregido y documentado

--- CCuenta.java
+++ CCuenta.java
@@ -1,14 +1,24 @@
+/**
+ * @author JonathanBetPer
+ * @since 02/06/2023
+ * @version v1
+ *
+ * Clase CCuenta.
+ * Dispone de los atributos y funciones necesarias para operar como una cuenta de un banco financiero
+ */
 public class CCuenta {
-
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
-    public CCuenta()
-    {
-    }
+
+    /**
+     * Constructor de la clase CCuenta
+     * (Vacío)
+     */
+    public CCuenta(){}
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
@@ -17,11 +27,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Método que devuelve el saldo de la cuenta
+     * @return saldo (double)
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Método que permite ingresar saldo a la cuenta
+     *  throws Exception ->  cantidad a ingresar negativa
+     * @param cantidad cantidad de saldo a ingresar
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -29,6 +48,12 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Método que permite retirar saldo de la cuenta
+     *  throws Exception ->  cantidad a retirar negativa
+     *                      saldo insuficiente
+     * @param cantidad cantidad de saldo a retirar
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -36,5 +61,57 @@ public class CCuenta {
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
+    }
+
+
+    /**
+     * Método que devuelve el nombre del dueño de la cuenta
+     * @return El nombre actual
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Método que devuelve el tipo de interés que tiene la cuenta (número con decimales)
+     * @return tipo de interés (double)
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Método que devuelve el IBAN asociado a la cuenta
+     * @return IBAN de la cuenta (String)
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+
+    /**
+     * Método que cambia el valor del número de la cuenta (IBAN
+     * @param cuenta IBAN (String)
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+
+    /**
+     * Método que devuelve el tipo de interés que tiene la cuenta (número con decimales)
+     * @return tipo de interés (double)
+     */
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+
+    /**
+     * Método que cambia el valor del tipo de interés
+     * @param tipoInteres tipo de interés (double)
+     */
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
     }
 }
--- Main.java
+++ Main.java
@@ -1,23 +1,53 @@
+/**
+ * @author JonathanBetPer
+ * @since 02/06/2023
+ * @version v1
+ *
+ *  Clase Main. App Banco qye controla los datos de una cuenta predeterminada
+ *
+ */
+
+
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        operativa_cuenta(0);
+    }
+
+    /**
+     * Método operativa_cuenta.
+     * Engloba todas las opciones de la clase CCuenta en el main
+     *
+     * @param cantidad atributo que pide la actividad
+     */
+    public static void operativa_cuenta(float cantidad){
+        //Variables
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        //Creacion Cuenta
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+
+        //Saldo actual
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
+
+        //Intenta retirar una cantidad, si salta una excepción sale un mensaje por consola
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
+
+
+        //Intenta retirar una cantidad, si salta una excepción sale un mensaje por consola
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
+
     }
 }
